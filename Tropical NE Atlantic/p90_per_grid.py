feaa2b245174cd98bd0edd45dff421ec5fd21334
Ajibola Aina <98769534+Ajibooy@users.noreply.github.com>
2026-03-25 22:54:57 +0100

    Update p90_per_grid.py

--- Tropical NE Atlantic/p90_per_grid.py
+++ Tropical NE Atlantic/p90_per_grid.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
-# Load final threshold file
-ds = xr.open_dataset("C:/Users/Aina Ajibola/Desktop/P90_1981-2010/threshold.nc")
+# Load data
+ds = xr.open_dataset(r"C:/Users/Aina Ajibola/Desktop/P90_1981-2010/threshold.nc")
 
-# STD over dayofyear
-std_p90 = ds.p90_threshold.std(dim="dayofyear")
+# Average P90 per grid cell
+mean_p90_per_grid = ds.p90_threshold.mean(dim="dayofyear")
 
 # Plot map
 fig = plt.figure(figsize=(10, 6))
 ax = plt.axes(projection=ccrs.PlateCarree())
 
-std_p90.plot(
+mean_p90_per_grid.plot(
     ax=ax,
     transform=ccrs.PlateCarree(),
-    cmap="viridis",
-    cbar_kwargs={"label": "STD of P90 (°C)"}
+    cmap="hot",
+    cbar_kwargs={"label": "P90 (°C)"}
 )
 
 ax.coastlines()
 ax.add_feature(cfeature.LAND, facecolor="lightgray")
 ax.add_feature(cfeature.BORDERS, linestyle=":")
 ax.gridlines(draw_labels=True)
-ax.set_title("STD of P90 over Day of Year (1981–2010)")
+ax.set_title("Average P90 per Grid Cell (1981–2010)")
 plt.show()
